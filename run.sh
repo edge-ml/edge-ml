eb7cbe310f282d4a3fc75aa0f9f1e34f46379f3c
Tobias King <tk.king.tk@gmail.com>
2021-06-09 09:59:27 +0200

    Edited run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 curl https://raw.githubusercontent.com/teco-kit/explorer/master/docker-compose-production.yaml > docker-compose-production.yaml && 
-docker-compose pull && 
 docker-compose -f docker-compose-production.yaml up -d && 
 rm docker-compose-production.yaml
