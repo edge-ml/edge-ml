46cf3197722bfc3d259ad86e1be733b97943ffaa
Tobias King <tk.king.tk@gmail.com>
2021-07-10 18:13:30 +0200

    Git commit -m "Added https to nginx"

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 curl https://raw.githubusercontent.com/teco-kit/explorer/master/docker-compose-production.yaml > docker-compose-production.yaml && 
-curl https://raw.githubusercontent.com/teco-kit/explorer/master/nginx.conf > nginx.conf &&
+curl https://raw.githubusercontent.com/teco-kit/explorer/master/nginx_https.conf > nginx_https.conf &&
 mkdir docs &&
+mkdir dockerVolume &&
+mkdir dhparam &&
 curl https://github.com/teco-kit/explorer/raw/master/docs/Datenbankschema_Explorer_v2.jpg > docs/Datenbankschema_Explorer_v2.jpg &&
 curl https://raw.githubusercontent.com/teco-kit/explorer/master/docs/index.html > docs/index.html
 docker-compose -f docker-compose-production.yaml pull && 
