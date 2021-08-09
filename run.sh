aab150a4c19abb8cf8c57fcc055418c015f903ec
Tobias King <tk.king.tk@gmail.com>
2021-08-09 15:46:47 +0200

    Adapted run-scripts to new repository

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
-curl https://raw.githubusercontent.com/teco-kit/explorer/master/docker-compose-production.yaml > docker-compose-production.yaml && 
-curl https://raw.githubusercontent.com/teco-kit/explorer/master/nginx_https.conf > nginx_https.conf &&
+curl https://raw.githubusercontent.com/edge-ml/edge-ml/master/docker-compose-production.yaml > docker-compose-production.yaml && 
+curl https://raw.githubusercontent.com/edge-ml/edge-ml/master/nginx_https.conf > nginx_https.conf &&
 mkdir docs &&
 mkdir dockerVolume &&
 mkdir dhparam &&
-curl https://github.com/teco-kit/explorer/raw/master/docs/Datenbankschema_Explorer_v2.jpg > docs/Datenbankschema_Explorer_v2.jpg &&
-curl https://raw.githubusercontent.com/teco-kit/explorer/master/docs/index.html > docs/index.html
+curl https://raw.githubusercontent.com/edge-ml/edge-ml/master/docs/index.html > docs/index.html
 docker-compose -f docker-compose-production.yaml pull && 
 docker-compose -f docker-compose-production.yaml up -d && 
 rm docker-compose-production.yaml
