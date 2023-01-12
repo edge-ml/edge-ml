252c17d93aa142211f8d4eb04767da224dfcf685
Murat Kurnaz <murat.kurnaz12@gmail.com>
2023-01-12 20:25:27 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/edge-ml/authentication
+npm install
+
+cd ~/edge-ml/backend
+npm install
+
+cd ~/edge-ml/frontend
+npm install
+
+cd ~/edge-ml/ml
+pip install -r requirements.txt
