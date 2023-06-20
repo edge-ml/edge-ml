817a7a52036e3857516f66e838512ae496d9b742
Tobias King <tk.king.tk@gmail.com>
2023-06-20 14:32:19 +0200

    Updated deploy config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git config --global --add safe.directory /home/ubuntu/edge-ml/backend
 git config --global --add safe.directory /home/ubuntu/edge-ml/frontend
 
-BRANCH_NAME = "main"
+BRANCH_NAME="main"
 
 
 cd /home/ubuntu/edge-ml
@@ -9,19 +9,19 @@ cd /home/ubuntu/edge-ml
 docker-compose -f docker-compose-production.yaml pull
 
 cd frontend
-git checkout BRANCH_NAME
+git checkout "$BRANCH_NAME"
 sudo -u ubuntu git pull
 cd ../ml
-git checkout BRANCH_NAME
+git checkout "$BRANCH_NAME"
 sudo -u ubuntu git pull
 cd ../backend
-git checkout BRANCH_NAME
+git checkout "$BRANCH_NAME"
 sudo -u ubuntu git pull
 cd ../authentication
-git checkout BRANCH_NAME
+git checkout "$BRANCH_NAME"
 sudo -u ubuntu git pull
 cd ../Dataset-store
-git checkout BRANCH_NAME
+git checkout "$BRANCH_NAME"
 sudo -u ubuntu git pull
 
 cd ..
