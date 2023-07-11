79d2a6591dec643b39db7e5e8e70baef293bd3ed
Tobias King <tk.king.tk@gmail.com>
2023-07-11 14:50:10 +0200

    Added firmwareCompiler to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,28 @@ git config --global --add safe.directory /home/ubuntu/edge-ml/frontend
 
 cd /home/ubuntu/edge-ml
 
-docker-compose -f docker-compose-production.yaml pull
-
 cd frontend
 git checkout beta
 sudo -u ubuntu git pull
+
 cd ../ml
 git checkout beta
 sudo -u ubuntu git pull
+
 cd ../backend
 git checkout beta
 sudo -u ubuntu git pull
+
 cd ../authentication
 git checkout beta
 sudo -u ubuntu git pull
+
 cd ../Dataset-store
 git checkout beta
+sudo -u ubuntu git pull
+
+cd ../firmwareCompile
+git checkout main
 sudo -u ubuntu git pull
 
 cd ..
