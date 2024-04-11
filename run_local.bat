736aeb6fef39533fe354a99afdbea1226642e0c5
Tobias King <tk.king.tk@gmail.com>
2024-04-11 16:32:31 +0200

    Working on script to run docker local

---
+++ run_local.bat
@@ -0,0 +1,12 @@
+mkdir edge-ml-beta
+
+REM Change directory to edge-ml-beta
+cd edge-ml-beta
+
+REM Download docker-compose-local.yaml file
+curl -o docker-compose-local.yaml https://raw.githubusercontent.com/edge-ml/edge-ml/beta/docker-compose-local.yaml
+curl -o .env-docker-local https://raw.githubusercontent.com/edge-ml/edge-ml/beta/.env-docker-local
+crul -o nginx.conf https://raw.githubusercontent.com/edge-ml/edge-ml/beta/nginx.conf
+
+docker-compose -f docker-compose-local.yaml pull
+docker-compose -f docker-compose-local.yaml up -d
