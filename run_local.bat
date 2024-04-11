d00f6f8c17030ab9cf0fece494b7dd25d86702f4
Tobias King <tk.king.tk@gmail.com>
2024-04-11 22:02:34 +0200

    Updated script to run locally

--- run_local.bat
+++ run_local.bat
@@ -6,7 +6,7 @@ cd edge-ml-beta
 REM Download docker-compose-local.yaml file
 curl -o docker-compose-local.yaml https://raw.githubusercontent.com/edge-ml/edge-ml/beta/docker-compose-local.yaml
 curl -o .env-docker-local https://raw.githubusercontent.com/edge-ml/edge-ml/beta/.env-docker-local
-crul -o nginx.conf https://raw.githubusercontent.com/edge-ml/edge-ml/beta/nginx.conf
+crul -o nginx.conf https://raw.githubusercontent.com/edge-ml/edge-ml/beta/nginx-local.conf
 
 docker-compose -f docker-compose-local.yaml pull
 docker-compose -f docker-compose-local.yaml up -d
