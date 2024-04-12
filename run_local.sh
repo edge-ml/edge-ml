6f1407219413f15ae1e54faa82c14654a4d3fe7b
Tobias King <tk.king.tk@gmail.com>
2024-04-12 09:16:17 +0200

    Added script to run locally on linux and macos

--- run_local.sh
+++ run_local.sh
@@ -1,9 +1,9 @@
 mkdir edge-ml-beta
 
-REM Change directory to edge-ml-beta
+# Change directory to edge-ml-beta
 cd edge-ml-beta
 
-REM Download docker-compose-local.yaml file
+# Download docker-compose-local.yaml file
 curl -o docker-compose-local.yaml https://raw.githubusercontent.com/edge-ml/edge-ml/beta/docker-compose-local.yaml
 curl -o .env-docker-local https://raw.githubusercontent.com/edge-ml/edge-ml/beta/.env-docker-local
 curl -o nginx.conf https://raw.githubusercontent.com/edge-ml/edge-ml/beta/nginx-local.conf
